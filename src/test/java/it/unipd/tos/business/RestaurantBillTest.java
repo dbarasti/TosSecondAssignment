9b4494a01744238483c49fc17a40862b8c19e0a0
dbarasti <dbarasti@gmail.com>
2018-12-11 00:48:05 +0100

    wrote some good tests, time to sleep

--- src/test/java/it/unipd/tos/business/RestaurantBillTest.java
+++
@@ -1,7 +0,0 @@
-package it.unipd.tos.business;
-
-import static org.junit.Assert.*;
-
-public class RestaurantBillTest {
-
-}
--- src/test/java/it/unipd/tos/business/RestaurantClientTest.java
+++ src/test/java/it/unipd/tos/business/RestaurantClientTest.java
@@ -1,19 +1,90 @@
 package it.unipd.tos.business;
 
 import it.unipd.tos.business.exception.RestaurantBillException;
+import it.unipd.tos.model.MenuItem;
+import it.unipd.tos.model.Type;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class RestaurantClientTest {
+    RestaurantClient restaurantClient = new RestaurantClient();
+    List<MenuItem> acceptedItemsOrderedAsList = new ArrayList();
+
+    List<MenuItem> moreThanTwentyItemsOrderedAsList = new ArrayList();
+
+    //builds an orderList
+    @Before
+    public void buildOrdersLists(){
+        acceptedItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Margherita", 4.70));
+        acceptedItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Pomodorini Rucola e Grana", 6.50));
+        acceptedItemsOrderedAsList.add(new MenuItem(Type.PRIMO, "Carbonara", 6.00));
+        acceptedItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Capricciosa", 6.80));
+        acceptedItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Speck e Grana", 7.20));
+        acceptedItemsOrderedAsList.add(new MenuItem(Type.PRIMO, "Amatriciana", 6.70));
+        acceptedItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Salame Piccante", 6.60));
+        acceptedItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Patatosa", 8.00));
+        acceptedItemsOrderedAsList.add(new MenuItem(Type.PRIMO, "Cacio e Pepe", 6.70));
+
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Margherita", 4.70));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Pomodorini Rucola e Grana", 6.50));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PRIMO, "Carbonara", 6.00));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Capricciosa", 6.80));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Speck e Grana", 7.20));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PRIMO, "Amatriciana", 6.70));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Salame Piccante", 6.60));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Patatosa", 8.00));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PRIMO, "Cacio e Pepe", 6.70));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Margherita", 4.70));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Pomodorini Rucola e Grana", 6.50));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PRIMO, "Carbonara", 6.00));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Capricciosa", 6.80));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Speck e Grana", 7.20));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PRIMO, "Amatriciana", 6.70));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Salame Piccante", 6.60));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Patatosa", 8.00));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PRIMO, "Cacio e Pepe", 6.70));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Margherita", 4.70));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Pomodorini Rucola e Grana", 6.50));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PRIMO, "Carbonara", 6.00));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Capricciosa", 6.80));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Speck e Grana", 7.20));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PRIMO, "Amatriciana", 6.70));
+        moreThanTwentyItemsOrderedAsList.add(new MenuItem(Type.PIZZA, "Salame Piccante", 6.60));
+
+    }
+
 
     @Test
-    public void getOrderPrice() {
-        RestaurantClient client = new RestaurantClient();
+    public void getOrderPrice_NullFirstParameter_returnsZero() {
         try {
-            assertEquals(0, client.getOrderPrice(null),0.0);
+            assertEquals(0, restaurantClient.getOrderPrice(null),0.0);
         } catch (RestaurantBillException e) {
             e.printStackTrace();
         }
     }
+
+    @Test(expected = RestaurantBillException.class)
+    public void getOrderPrice_MoreThanTwentyOrderItems_ExceptionThrown() throws RestaurantBillException {
+
+        restaurantClient.getOrderPrice(moreThanTwentyItemsOrderedAsList);
+
+    }
+
+    @Test
+    public void getOrderPrice_NoDiscountAppliedOnBill_correctBillSum(){
+        double assertionResult = acceptedItemsOrderedAsList.stream()
+                .mapToDouble(MenuItem::getItemPrice)
+                .sum();
+        try {
+            assertEquals(assertionResult, restaurantClient.getOrderPrice(acceptedItemsOrderedAsList),0.0);
+        } catch (RestaurantBillException e) {
+            e.printStackTrace();
+        }
+    }
+
 }

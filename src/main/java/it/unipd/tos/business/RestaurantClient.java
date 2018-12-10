0c0cc0316b77c32858859d18fc5325a925ef3e56
Davide <dbarasti@gmail.com>
2018-12-10 13:28:58 +0100

    completed project structure, now implementing

---
+++ src/main/java/it/unipd/tos/business/RestaurantClient.java
@@ -0,0 +1,13 @@
+package it.unipd.tos.business;
+
+import it.unipd.tos.business.exception.RestaurantBillException;
+import it.unipd.tos.model.MenuItem;
+
+import java.util.List;
+
+public class RestaurantClient implements RestaurantBill {
+    @Override
+    public double getOrderPrice(List<MenuItem> itemsOrdered) throws RestaurantBillException {
+        return 0;
+    }
+}
--- src/main/java/it/unipd/tos/model/MenuItem.java
+++ src/main/java/it/unipd/tos/model/MenuItem.java
@@ -4,4 +4,13 @@
 package it.unipd.tos.model;
 
 public class MenuItem {
+    private final Type itemType;
+    private String name;
+    private Float price;
+
+    public MenuItem(Type itemType, String name, Float price) {
+        this.itemType = itemType;
+        this.name = name;
+        this.price = price;
+    }
 }
